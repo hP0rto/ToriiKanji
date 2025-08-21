78d105c503b3db2b26f3739a9ef290eb18377c08
BartH28 <henriquelb28@gmail.com>
2025-08-21 20:19:21 +0000

    Revert "Add splashScreen and Tray."
    This reverts commit d71c3cee084bb88107ba2d8138e89b3c38e9ddcc.

--- app/main.py
+++ app/main.py
@@ -1,36 +1,12 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QSplashScreen
-from PyQt6.QtGui import QPixmap, QColor
+from PyQt6.QtWidgets import QApplication,QStackedWidget
 from gui.MainWindow import MainWindow
-from PyQt6.QtCore import Qt, QTimer
-from utils.paths import BACKGROUND_IMG
+from gui.OverlayPanel import OverlayPanel
+from gui.SettingsPanel import SettingsPanel
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    
-    splash_pix = QPixmap(str(BACKGROUND_IMG)) 
-    splash = QSplashScreen(splash_pix, Qt.WindowType.FramelessWindowHint)
-    splash.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint)
-    splash.setEnabled(False)
-    splash.show()
+    main = MainWindow()
+    main.show()
 
-    progress = {"value": 0}
-
-    def update_progress():
-        progress["value"] += 5
-        splash.showMessage(
-            f"Loading... {progress['value']}%",
-            Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignCenter,
-            QColor("white")
-        )
-        if progress["value"] >= 100:
-            timer.stop()
-            main = MainWindow()
-            main.show()
-            splash.finish(main)  
-
-    timer = QTimer()
-    timer.timeout.connect(update_progress)
-    timer.start(50)
-    
     sys.exit(app.exec())
